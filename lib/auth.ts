f10bcae847e24a6d0bf4c16c5a93c4ffc2557695
AngcGabriel-rgb <gabrielange433@gmail.com>
2025-07-02 14:50:46 +0100

    produit final 1

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,7 @@
 import type { LoginCredentials, RegisterData, AuthResponse, ApiResponse } from "./types"
 
 // Remplacer l'ancienne URL de l'API par la nouvelle
-const API_BASE_URL = "https://idea-r1ff.onrender.com"
+const API_BASE_URL = "https://idea1-j5f4.onrender.com"
 
 // Fonction utilitaire pour les requêtes API
 async function apiRequest<T>(endpoint: string, options: RequestInit = {}): Promise<ApiResponse<T>> {
